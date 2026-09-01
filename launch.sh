7de1c0f0d7c98dc392ea92b6843e399c893cab86
JorgeNavarro <jorge.85navarro@gmail.com>
2026-09-01 15:43:17 -0600

    Launch for unix updated

--- launch.sh
+++ launch.sh
@@ -1,1 +1,7 @@
-clear && g++ main.cpp Graph.cpp Node.cpp -o Output.exe && ./Output.exe
+if command -v cmake &> /dev/null; then
+    # Running with cmake
+    # Clear the current terminal, specify the current directory as . and the build directory as build, then run the output
+    clear && cmake -S . -B build && cmake --build build && ./build/Output
+else
+    clear && mkdir build && g++ main.cpp Graph.cpp Node.cpp -o ./build/Output.exe &&  ./build/Output.exe
+fi
